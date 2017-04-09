77f02276344e493ea461ec58e4c602887289764a
Edwin Vargas <onix.edwin18@gmail.com>
2017-04-09 19:09:17 -0400

    Pule y actualiza acciones para traer la data correctamente
    - Se agrega fetchData y fetchingData para manejo de las peticiones
    - fetchingData se usa para (mediante thunks) ejecutar varias acciones
    - Se agregan las constantes requeridas en el archivo constants

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,4 +1,6 @@
-import { SET_TOKEN_ACCESS, UPDATE_SEARCH_RESULTS, UPDATE_PROFILE, APPEND_POSTS } from './constants';
+import { SET_TOKEN_ACCESS, UPDATE_SEARCH_RESULTS, UPDATE_PROFILE, UPDATE_MEDIA, APPEND_POSTS, FETCHING_DATA, PROFILE, SEARCH, MEDIA } from './constants';
+
+import fetchJsonp from 'fetch-jsonp';
 
 //Agrega el token de acceso
 export const setTokenAccess = (token) => {
@@ -21,14 +23,64 @@ export const updateProfile = (data, media) => {
   return {
     type: UPDATE_PROFILE,
     data,
-    media
+  }
+}
+
+//Establece los posts recientes
+export const updateMedia = (data) => {
+  return {
+    type: UPDATE_MEDIA,
+    data,
   }
 }
 
 //Añade mas posts
-export const appendPosts = (posts) => {
+export const appendPosts = (data) => {
   return {
     type: APPEND_POSTS,
-    posts
+    data
   }
+}
+
+//Me dice si estoy o no trayendo data
+export const fetchingData = () => {
+  return {
+    type: FETCHING_DATA,
+  }
+}
+
+export function fetchData(url, type, append = false) {
+
+  return (dispatch) => {
+    dispatch(fetchingData());
+
+    fetchJsonp(url)
+      .then((response) => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+
+        dispatch(fetchingData());
+        return response;
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        switch (type) {
+          case SEARCH:
+            dispatch(updateSearchResults(data));
+            break;
+          case PROFILE:
+            dispatch(updateProfile(data));
+            break;
+          case MEDIA:
+            if(!append)
+              dispatch(updateMedia(data));
+            else
+              dispatch(appendPosts(data));
+            break;
+          default:
+            return {};
+        }
+      });
+  };
 }
